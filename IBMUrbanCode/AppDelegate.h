e03fc178847b781d93c0c1beaf29e1138d8b1d75
TCSUnited <UnitedTCS@Uniteds-iMac.local>
2017-04-02 19:33:19 +0530

    first commit

---
+++ IBMUrbanCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBMUrbanCode
+//
+//  Created by United TCS on 4/2/17.
+//  Copyright © 2017 United TCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
